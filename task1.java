af977538f249c91e74a2aa19e5c729e8a3bf90f5
anastace <60660859+anastace@users.noreply.github.com>
2021-12-27 23:49:35 +0200

    Update task1.java

--- task1.java
+++ task1.java
@@ -1,81 +1,50 @@
-package subtask1.atomicTypeTransactions;
+package com.example.task1;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicReference;
 
-import java.util.Arrays;
-import java.util.concurrent.atomic.AtomicInteger;
+public class CasMutex extends Object {
+    private AtomicReference<Runnable> currThread = new AtomicReference<>();
+    private LinkedBlockingQueue<Runnable> waitingThreads;
 
-class Bank {
-    public static final int TEST_COUNT = 10000;
-    private final AtomicInteger[] accounts;
-    private long transactionCount = 0;
-
-    public Bank(int accountCount, int initialBalance) {
-        accounts = new AtomicInteger[accountCount];
-        for (int i = 0; i < accounts.length; i++) {
-            accounts[i] = new AtomicInteger(initialBalance);
+    public CasMutex(LinkedBlockingQueue<Runnable> waitingThreads) {
+        this.waitingThreads = waitingThreads;
+    }
+    public void lock(){
+        if (Thread.currentThread().equals(currThread.get())) {
+            throw new RuntimeException("locked");
         }
-        transactionCount = 0;
-    }
-
-    public void transfer(int from, int to, int amount) throws InterruptedException {
-        accounts[from].addAndGet(-amount);
-        accounts[to].addAndGet(amount);
-        transactionCount++;
-        if (transactionCount % TEST_COUNT == 0) {
-            test();
+        while (!currThread.compareAndSet(null, Thread.currentThread())) {
+            Thread.yield();
         }
+        System.out.println("locked by: " + Thread.currentThread().getName());
     }
-    public void test() {
-        int sum = 0;
-        for (AtomicInteger account : accounts) {
-            sum += account.get();
+    public void unlock() {
+        if (!Thread.currentThread().equals(currThread.get())) {
+            throw new RuntimeException("not locked");
         }
-        System.out.println("Transactions:" + transactionCount + " Sum: " + sum);
+        System.out.println("unlocked by: " + Thread.currentThread().getName());
+        currThread.set(null);
     }
-
-    public int size() {
-        return accounts.length;
-    }
-}
-
-
-public class BankTest {
-    public static final int ACCOUNT_COUNT = 10;
-    public static final int INITIAL_BALANCE = 10000;
-
-    public static void main(String[] args) {
-        Bank bank = new Bank(ACCOUNT_COUNT, INITIAL_BALANCE);
-        int i;
-        for (i = 0; i < ACCOUNT_COUNT; i++) {
-            TransferThread transferThread = new TransferThread(bank, i, INITIAL_BALANCE);
-            transferThread.setPriority(Thread.NORM_PRIORITY + i % 2);
-            transferThread.start();
+    public void casWait() throws InterruptedException {
+        Thread thread = Thread.currentThread();
+        if (!thread.equals(currThread.get())) {
+            throw new RuntimeException("not locked");
         }
+        waitingThreads.put(thread);
+        System.out.println("Wait: " + Thread.currentThread().getName());
+        unlock();
+        while (waitingThreads.contains(thread)) {
+            Thread.yield();
+        }
+        lock();
+        System.out.println("No wait: " + Thread.currentThread().getName());
     }
-}
-
-
-class TransferThread extends Thread {
-    private final Bank bank;
-    private final int fromAccount;
-    private final int maxAmount;
-    private static final int REPETITIONS = 10000;
-
-    public TransferThread(Bank bank, int from, int max) {
-        this.bank = bank;
-        fromAccount = from;
-        maxAmount = max;
+    public void casNotify() throws InterruptedException {
+        waitingThreads.take();
+        System.out.println("Notify: " + Thread.currentThread().getName());
     }
-
-    public void run() {
-        try {
-            while (!interrupted()) {
-                for (int i = 0; i < REPETITIONS; i++) {
-                    int toAccount = (int)(bank.size() * Math.random());
-                    int amount = (int)(maxAmount * Math.random() / REPETITIONS);
-                    bank.transfer(fromAccount, toAccount, amount);
-                    Thread.sleep(1);
-                }
-            }
-        } catch (InterruptedException ignored) {}
+    public void casNotifyAll() {
+        waitingThreads.clear();
+        System.out.println("Notify all: " + Thread.currentThread().getName());
     }
 }
